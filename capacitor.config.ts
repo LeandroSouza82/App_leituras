c1f3d8507cea740a7195a2f9652be45c1841e531
LeandroSouza82 <lsouza557@gmail.com>
2026-08-06 18:43:41 -0300

    fix: adiciona workflow automatico para build do APK

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.leiturasapp.app',
+  appName: 'LeiturasApp',
+  webDir: 'dist'
+};
+
+export default config;
